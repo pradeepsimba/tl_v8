d2691dd51ceb48aeb8120c5fc09f7445ca79b773
pradeepsimba <96039278+pradeepsimba@users.noreply.github.com>
2024-11-22 15:21:45 +0530

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,7 @@
+    path('error_page1/', da1_error_data_view, name='error_page1'),
+    path('error_page2/', da2_error_data_view, name='error_page2'),
+    path('error_page3/', da3_error_data_view, name='error_page3'),
+    
+    path('tlerror_page1/', tl_da1_error_data_view, name='tlerror_page1'),
+    path('tlerror_page2/', tl_da2_error_data_view, name='tlerror_page2'),
+    path('tlerror_page3/', tl_da3_error_data_view, name='tlerror_page3'),
