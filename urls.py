136e7881a9b8721fbc3d94b4f95ed92349d65b22
pradeepsimba <96039278+pradeepsimba@users.noreply.github.com>
2024-11-25 12:35:01 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -5,3 +5,7 @@
     path('tlerror_page1/', tl_da1_error_data_view, name='tlerror_page1'),
     path('tlerror_page2/', tl_da2_error_data_view, name='tlerror_page2'),
     path('tlerror_page3/', tl_da3_error_data_view, name='tlerror_page3'),
+
+    path('tl2error_page1/', tl2_da1_error_data_view, name='tl2error_page1'),
+    path('tl2error_page2/', tl2_da2_error_data_view, name='tl2error_page2'),
+    path('tl2error_page3/', tl2_da3_error_data_view, name='tl2error_page3'),
